a8511aa8cc59da3b8617cb77e5be5dad316c9acb
carsonalder1 <carsonalder1@gmail.com>
2026-04-22 19:58:02 -0600

    Prep 1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name? ");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("What is your Last Name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"I see. Your name is {lastname}, {firstname} {lastname}. ");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello! Please put in the grade percentage you got for the class: ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+            {
+                Console.WriteLine("You got an A ");
+            }
+        else if (grade >= 80)
+            {
+                Console.WriteLine("You got a B ");
+            }
+        else if (grade >= 70)
+            {
+                Console.WriteLine("You got a C ");
+            }
+        else if (grade >= 60)
+            {
+                Console.WriteLine("You got a D ");
+            }
+        else
+            {
+                Console.WriteLine("You got an F ");
+            }
     }
 }
