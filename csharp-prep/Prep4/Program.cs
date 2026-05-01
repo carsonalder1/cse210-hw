4a97047f36b437ce1075639007d8e003e300419a
carsonalder1 <carsonalder1@gmail.com>
2026-05-01 11:56:21 -0600

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Globalization;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Add some numbers to a list. When you are done type 0");
+        
+        int userInput = -1;
+        
+        while (userInput != 0)
+        {
+            Console.Write("Enter a number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        if (numbers.Count > 0)
+        {
+            double mean = (double)sum / numbers.Count;
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {mean}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered");
+        }
     }
 }
