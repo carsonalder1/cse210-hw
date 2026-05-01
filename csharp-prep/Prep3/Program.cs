ca759ddd3020af04d3bfc96659be2c08819a7afc
carsonalder1 <carsonalder1@gmail.com>
2026-04-30 18:22:53 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        float magicNumber = float.Parse(Console.ReadLine());
+        
+        float guessNumber;
+
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            guessNumber = float.Parse(Console.ReadLine());
+            {
+            if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+        } while (guessNumber != magicNumber);
+
+        Console.WriteLine("You Got It!");
     }
 }
