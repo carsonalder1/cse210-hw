01ffb70e1b76b0878a4b3d6b494ff669b59b7bec
carsonalder1 <carsonalder1@gmail.com>
2026-05-04 14:36:33 -0600

    Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle= "Zoo Worker";
+        job2._company = "PHX Zoo";
+        job2._startYear = 2022;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._userName = "Carson Alder";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
+    
 }
+
